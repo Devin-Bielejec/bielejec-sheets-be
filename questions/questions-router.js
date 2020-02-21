27276244872075187d76b4f043ed514fd1ea84b0
devin-bielejec <devin.bieleje@gmail.com>
2020-02-21 17:18:50 -0500

    questions router get by function works

--- questions/questions-router.js
+++ questions/questions-router.js
@@ -4,6 +4,18 @@ const Questions = require("../questions/questions-model.js");
 router.post("/questionsByFilter", (req, res) => {
   const changes = req.body;
 
+  //ensure that user is sending only correct column names
+  let correctColumns = ["topic", "subject", "standard", "type"];
+
+  for (let key in changes) {
+    if (!correctColumns.includes(key) || !Array.isArray(changes[key])) {
+      res.status(404).json({
+        message:
+          "You need to include 'topic: [stuff]', 'subject: [stuff]', 'standard: [stuff]', or 'type: [stuff]'"
+      });
+    }
+  }
+
   Questions.getByFilter(changes)
     .then(data => {
       if (data) {
