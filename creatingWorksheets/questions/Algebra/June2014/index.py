1f9f950d4cafc876dc0cd7ef074191be80e0697c
Devin Bielejec <devin.bielejec@gmail.com>
2020-05-08 15:50:49 -0400

    latexmk and texlive installed locally - question works

--- creatingWorksheets/questions/Algebra/June2014/index.py
+++ creatingWorksheets/questions/Algebra/June2014/index.py
@@ -3,6 +3,10 @@ import uuid
 from pylatex import (Document, TikZ, TikZNode,
                      TikZDraw, TikZCoordinate,
                      TikZUserPath, TikZOptions)
+from pylatex.utils import (NoEscape)
+import sys
+sys.path.insert(0, "creatingWorksheets/utils")
+from questionFormatting import multipleChoice
 
 # __all__ = ["June2014_1", "June2014_12"]
 
@@ -22,12 +26,22 @@ class June2014_1():
         #This can get convoluted, so maybe don't worry about it so much right now
         self.skills = ["some major skill", "some other major skill"]
 
+        self.correctAnswer = None
+
+        self.solution = None
+
     def addQuestion(self, doc = None):
         #Junk here where you add to the document
         """
         When solving the equation A(Bx^2 + C) - D = Ex^2 + F, {name} wrote A(Bx^2 + C) = Ex^2 + F + D as her first step. Which property justifies {name}'s first step?
         A -> random numbeer between 2, 9
         B -> random number between 2, 9
+
+        Choices:
+        1) Addition property of equality
+        2) commutative property of equality
+        3) multiplication property of equality
+        4) distributive property of equatlity
         """
         a = random.randint(2,9)
         b = random.randint(2,9)
@@ -39,15 +53,29 @@ class June2014_1():
         name = random.choice(["Emily", "David", "Jakim", "Joe", "John"])
 
         #Let's see if it works without anything in particular
-        equationString = rf"{a}(x^{2}+{c})-{d}={e}x^{2}"
-        doc.append("Hi")
+        equation1 = rf"${a}({b}x^{2}+{c})-{d}={e}x^{2}+{f}$"
+        equation2 = rf"${a}({b}x^{2}+{c})={e}x^{2}+{f+d}$"
+        
+        wholeString = f"When solving the equation {equation1}, {name} wrote {equation2}, as their first step. Which property justifies {name}'s first step"
 
+        #Choice 1 is always correct answer
+        choice1 = "Addition property of equality"
+        choice2 = "Commutative property of equality"
+        choice3 = "Multiplication property of equality"
+        choice4 = "Distributive property of equality"
 
+        #Adds question
+        doc.append(NoEscape(wholeString))
+
+        #Adds choices - returns correct answer
+        self.correctAnswer = multipleChoice(choices = [choice1, choice2, choice3, choice4], doc = doc)
+        
     def addAnswer(self, doc = None):
-        #Junk here where you add to the document
+        doc.append(NoEscape(self.correctAnswer))
         pass
     
     def addSolution(self, doc = None):
+
         pass
         #Junk here where you would add to the solution
 
@@ -172,7 +200,7 @@ class June2014_3():
 
         #Let's see if it works without anything in particular
         equationString = rf"{a}(x^{2}+{c})-{d}={e}x^{2}"
-        doc.append("Hi")
+        doc.append("Hid from you")
 
 
     def addAnswer(self, doc = None):
---
+++ creatingWorksheets/utils/questionFormatting.py
@@ -0,0 +1,30 @@
+import random
+from pylatex.utils import NoEscape
+from pylatex import VerticalSpace, NewLine
+
+#Always make first choice correct answer
+def multipleChoice(choices = ['1','2','3','4','5'], fifthVariation = False, doc = None):
+    correctAnswer = choices[0]
+
+    if len(choices) != len(set(choices)):
+        return "ERROR"
+
+    #All choices will be shuffled - not numerical
+    random.shuffle(choices)
+
+    for i in range(len(choices)):
+        doc.append(NoEscape('(%s) ' % str(i+1) + choices[i]))
+        if i == 0:
+            doc.append(VerticalSpace('.05in'))
+
+        if i != 3:
+            doc.append(NewLine())
+            doc.append(VerticalSpace('.05in'))
+
+    if fifthVariation == True:
+        doc.append(NoEscape(r'(%d) I do not know. (Worth $\frac{1}{3}$ points)' % (len(choices)+1)))
+
+    #Retrun correct answer
+    for choice in choices:
+        if choice == correctAnswer:
+            return choices.index(choice) + 1
